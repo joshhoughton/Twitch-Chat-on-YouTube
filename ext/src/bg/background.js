5e888c269e04375c04e48c26379182e0b337743c
Josh Houghton <shubbler@me.com>
2021-11-27 01:14:12 +0000

    v0.0.1

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -1,7 +1,6 @@
-// background.js
 chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
       target: {tabId: tab.id},
-      files: ['src/bg/content.js',"js/jquery/jquery.min.js"]
+      files: ["js/jquery/jquery.min.js", "src/bg/content.js"]
     });
   });
--- ext/src/bg/content.js
+++ ext/src/bg/content.js
@@ -1,16 +1,49 @@
-username = prompt("Enter a Twitch username:", "esl_csgo");
+suggestions = {
+    "esl": "esl_csgo",
+    "blast": "blastpremier"
+}
 
-ytChat = $("ytd-live-chat-frame")
+function getSuggestion(){
+    title = document.getElementsByTagName('title')[0].text.toLowerCase()
+    
+    suggestion = undefined
+    
+    for (const s in suggestions){
+        console.log(s)
+        console.log(title)
+        if (title.includes(s)){
+            suggestion = suggestion[s]
+        }
+    }
 
-ytChat.empty()
+    return suggestion
+}
 
-ytChat.css({
-    "flex-direction": "row",
-    "-webkit-flex-direction": "row"
-})
+function showTwitchChat() {
+    ytChat = $("ytd-live-chat-frame")
+
+    ytChat.empty()
+    
+    ytChat.css({
+        "flex-direction": "row",
+        "-webkit-flex-direction": "row"
+    })
+    
+    
+    if (document.documentElement.getAttribute("dark")){
+        url = `https://www.twitch.tv/embed/${username}/chat?${document.documentElement.getAttribute("dark") ? "darkpopout&" : ""}parent=www.youtube.com"`
+    }
+    
+    ytChat.prepend(
+        `<iframe style="flex: auto;" src="${url}">
+        </iframe>`
+    )
+    
+}
 
 
-ytChat.prepend(
-    `<iframe style="flex: auto;" src="https://www.twitch.tv/embed/${username}/chat?parent=www.youtube.com">
-    </iframe>`
-)
+username = prompt("Enter a Twitch username:", getSuggestion());
+
+if (username) {
+    showTwitchChat()
+}
